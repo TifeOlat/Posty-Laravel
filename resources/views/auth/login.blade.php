cdff0ccd18bdc5d2a2ae3b9169f4a500f96abde5
TifeOlat <boluolat@yahoo.co.uk>
2021-04-14 14:48:56 +0100

    first commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,46 @@
+@extends('layout.app')
+@section('content')
+
+    <div class="flex justify-center">
+        <div class="w-4/12 bg-white p-6 rounded-lg">
+        @if (session('status'))
+            <div class="bg-red-500 p-4 rounded-lg mb-6 text-white text-center">
+                {{ session('status') }}
+            
+            </div>
+        @endif
+            <form action="{{ route('login') }}" method="post">
+            @csrf
+                
+                <div>
+                    <label for="email" class="sr-only">Email</label>
+                    <input type="email" name="email" id="email" placeholder="Your Email" class="bg-gray-100 border-2 w-full p-4 rounded-lg mb-3 @error('email') border-red-500 @enderror" value="{{ old('email')}}">
+                    @error('email')
+                        <div class="text-red-500 mt-2 mb-2 text-sm">
+                            {{ $message }}
+                        </div>
+
+                    @enderror
+                </div>
+                <div>
+                    <label for="password" class="sr-only">Password</label>
+                    <input type="password" name="password" id="password" placeholder="Choose a Password" class="bg-gray-100 border-2 w-full p-4 rounded-lg mb-3" >
+                    @error('password')
+                        <div class="text-red-500 mt-2 mb-2 text-sm">
+                            {{ $message }}
+                        </div>
+
+                    @enderror
+                </div>
+                <div>
+                    <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full">Login</button>
+                </div>
+            </form>
+        </div>
+
+    </div>
+
+
+
+
+@endsection
